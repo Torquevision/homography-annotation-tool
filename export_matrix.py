[FIX] scaling after reload

export_matrix.py
@@ -13,18 +13,22 @@ def main(args):
         file_path = os.path.join(args.data_path, file)
         src_points, dest_points = [], []
         for i in range(0, len(data[file]['points']), 2):
+            height, width = data[file]['height'], data[file]['width']
             src_points.append(data[file]['points'][i])
-            dest_points.append(data[file]['points'][i+1])
+            x_, y_ = data[file]['points'][i+1]
+            # for horizontal stacking
+            dest_points.append([x_ - width, y_])
         
         src_points = np.array(src_points)
         dest_points = np.array(dest_points)
+        #print(src_points, dest_points)
         h = None
         if len(src_points) >= 4:
             h, status = cv2.findHomography(src_points, dest_points)
 
         if h is not None:
             s = ' '.join([file] + [str(k) for k in h.reshape(9).tolist()][:-1])
-            #print(s)
+            print(s)
             w.write(s + '\n')
         else:
             print('--> Skipping ', file, h)

main.py
@@ -112,12 +112,12 @@ def main(args):
         y_ = vis.shape[0]
         x_ = vis.shape[1]
         targetSizeX, targetSizeY = x_//2, y_//2
-        print(targetSizeX, targetSizeY)
         x_scale = x_ / targetSizeX 
         y_scale = y_ / targetSizeY
         vis = cv2.resize(vis, (targetSizeX, targetSizeY))
         image = vis.copy()
-        global_points = read_data(file, file_name = output_path)
+        pts = read_data(file, file_name = output_path)
+        global_points = [[int(np.round(x / x_scale)), int(np.round(y / y_scale))] for x, y in pts]
         cv2.imshow('Image', image)
         update_all_view(global_points)
         fuck_this_loop = False
@@ -133,10 +133,7 @@ def main(args):
             elif key ==  ord('f'):
                 fuck_this_loop = True
                 break
-        print(x_scale, y_scale)
         new_points = [(int(np.round(x * x_scale)), int(np.round(y * y_scale))) for x,y in global_points]
-        print(global_points)
-        print(new_points)
         add_data(file, new_points, img.shape, file_name = output_path)
         if fuck_this_loop:
             break